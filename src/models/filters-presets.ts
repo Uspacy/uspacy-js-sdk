529f95c42565ed30a166c623521560570d0b991a
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2023-10-12 00:03:58 +0300

    save changes

---
+++ src/models/filters-presets.ts
@@ -0,0 +1,14 @@
+import { IFilterRegularTasks, IFilterTasks } from './tasks';
+
+export interface IFilterField {
+	id: number;
+	title: string;
+	checked: boolean;
+}
+
+export interface IFilterPreset {
+	title: string;
+	filters: IFilterTasks | IFilterRegularTasks;
+	pinned: boolean;
+	filterFields: IFilterField[];
+}
