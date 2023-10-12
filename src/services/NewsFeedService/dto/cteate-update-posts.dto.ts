4bdf302fa2cccda05ca4422a3b7103c44ad2c083
Alexander Solomahin <alexsolomahin377@gmail.com>
2023-10-12 16:39:07 +0300

    remove unused

--- src/services/NewsFeedService/dto/cteate-update-posts.dto.ts
+++ src/services/NewsFeedService/dto/cteate-update-posts.dto.ts
@@ -1,25 +1,5 @@
 import { EntryType } from 'perf_hooks';
 
-// import { IComment } from '../../../models/comment';
-// import { EmotionType } from '../../../models/newsfeed';
-
-// export interface Post {
-// 	id?: string;
-// 	title?: string;
-// 	message?: string;
-// 	authorId?: string;
-// 	author_mood?: EmotionType;
-// 	files?: FileInfoDto[];
-// 	date?: number;
-// 	comments?: IComment[];
-// 	recipients?: RecipientsPost;
-// 	reactions?: {
-// 		reaction: number;
-// 		amount: number;
-// 		entityId: number;
-// 	}[];
-// }
-
 export interface FileInfoDto {
 	id: number;
 	entityType: EntryType[] | string;
