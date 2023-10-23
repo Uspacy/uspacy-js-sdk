6b04ce0ce14dce68b3206154ce58fdb6eed2d4cf
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2023-10-23 17:17:17 +0300

    updated types

--- src/models/tasks.ts
+++ src/models/tasks.ts
@@ -85,23 +85,8 @@ export interface IFilterTasks {
 	accomplices?: number[];
 	auditors?: number[];
 	deadline?: number[][];
-	openCalendar?: boolean;
-	search?: string;
-}
-
-export interface IFilterRegularTasks {
-	total?: number;
-	page?: number;
-	perPage?: number;
-	status?: string[];
-	priority?: string[];
-	time_label?: string[];
-	certainDateOrPeriod?: number[];
-	createdBy?: number[];
-	responsible?: number[];
-	accomplices?: number[];
-	auditors?: number[];
-	deadline?: number[][];
+	accept_request?: boolean[];
+	closed_by?: number[];
 	openCalendar?: boolean;
 	search?: string;
 }
@@ -117,6 +102,8 @@ export interface ITasksParams {
 	search?: string;
 	q?: string;
 	groupId?: number;
+	accept_request?: boolean[];
+	closed_by?: number[];
 	page?: number;
 	list?: number;
 	template?: 1;
