c60da76e242f96f903daddf14566d8949cf0fd24
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2023-12-28 17:42:28 +0200

    Updated types for email

--- src/services/EmailService/create-email.dto.ts
+++ src/services/EmailService/create-email.dto.ts
@@ -8,21 +8,13 @@ export interface IContactsForCreateLetter {
 
 export interface IAttachmentsForCreateLetter {
 	file_id: number;
-	file_name: string;
-	file_url: string;
-	creator_id: number;
-	entity_type: string;
-	entity_id: number;
-	upload_id: number;
-	original_filename: string;
-	last_modified: number;
-	size: number;
 }
 
 export interface ICreateLetterPayload {
 	subject?: string;
 	body?: string;
 	body_html?: string;
+	is_read?: boolean;
 	contacts?: IContactsForCreateLetter[];
 	attachments?: IAttachmentsForCreateLetter[];
 	imap_mapping?: IImapMapping;
