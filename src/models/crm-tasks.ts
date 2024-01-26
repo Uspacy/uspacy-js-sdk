bf37e2620053d89f5e1481964af86c56350d4229
Ivan Mukoed <1rydler@gmail.com>
2024-01-26 11:08:39 +0200

    added tasks store

---
+++ src/models/crm-tasks.ts
@@ -0,0 +1,44 @@
+import { IEntityData, IMeta } from './crm-entities';
+import { INotify } from './notify';
+
+export interface ITasks {
+	data: ITask[];
+	meta: IMeta;
+	aborted: boolean;
+}
+interface IEntityInfo {
+	id: number;
+	title: string;
+	owner: number;
+	source: string;
+	avatar?: string;
+	noAccess?: boolean;
+}
+
+export interface ITask {
+	id: number;
+	created_by: number;
+	title: string;
+	type: 'task' | 'call' | 'meeting' | 'chat' | 'email';
+	status: 'planned' | 'done' | 'cancelled';
+	description: string;
+	start_time: number;
+	end_time: number;
+	responsible_id: number;
+	company_id: number;
+	participants: number[];
+	contacts: IEntityInfo[];
+	deals: IEntityInfo[];
+	leads: IEntityInfo[];
+	company: IEntityInfo;
+	entity?: IEntityData | [];
+	noAccess?: boolean;
+	mentioned?: INotify;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	entities?: any;
+}
+
+export interface ICall extends ITask {
+	incomingPhone: string;
+	outgoingPhone: string;
+}
--- src/services/CrmCompaniesService/index.ts
+++ src/services/CrmCompaniesService/index.ts
@@ -82,7 +82,7 @@ export class CrmCompaniesService {
 	 * @param settings editing settings
 	 */
 	massCompaniesEditing({ entityIds, exceptIds, all, params, payload, settings }: IMassActions) {
-		return this.httpClient.client.patch<IEntityData>(`${this.namespace}/mass_edit${params}`, {
+		return this.httpClient.client.patch(`${this.namespace}/mass_edit${params}`, {
 			all,
 			entity_ids: entityIds,
 			except_ids: exceptIds,
