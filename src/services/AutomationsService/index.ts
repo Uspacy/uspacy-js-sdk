c6d4a0d94dfd50678f0e7fa29cbf3314b34ac8ee
Alexander <53402125+RealSolomon@users.noreply.github.com>
2024-07-12 15:24:51 +0300

    [2186] feat: add automations (#70)
    Co-authored-by: MysikM <46257801+MysikM@users.noreply.github.com>

---
+++ src/services/AutomationsService/index.ts
@@ -0,0 +1,48 @@
+import { injectable } from 'tsyringe';
+
+import { HttpClient } from '../../core/HttpClient';
+import { IApp } from '../../models/app';
+import { IAutomation } from '../../models/automations';
+import { IResponseWithMeta } from '../../models/response';
+
+/**
+ * Automations service
+ */
+@injectable()
+export class AutomationsService {
+	private namespace = '/automations-backend/v1/';
+	constructor(private readonly httpClient: HttpClient) {}
+
+	/**
+	 * Get automations list
+	 * @param page page number
+	 * @param list page count
+	 * @param search search query
+	 */
+	async getAutomations(page?: number, list?: number, search?: string) {
+		return this.httpClient.client.get<IResponseWithMeta<IApp[]>>(this.namespace, {
+			params: {
+				page,
+				list,
+				search,
+			},
+		});
+	}
+
+	/**
+	 * Delete automation
+	 * @param id automation id
+	 */
+	deleteAutomation(id: number) {
+		return this.httpClient.client.delete<number>(`${this.namespace}:id`, { urlParams: { id } });
+	}
+
+	/**
+	 * Toggle automation
+	 * @param id automation id
+	 * @param body request body
+	 */
+	toggleAutomation(id: number, body: IAutomation) {
+		return this.httpClient.client.patch(`${this.namespace}:id`, body, { urlParams: { id } });
+	}
+}
