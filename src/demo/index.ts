659c8d59af4a045d010b1602259f3184b8ea6e4c
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2024-07-17 17:56:28 +0300

    Removed console

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -4,9 +4,7 @@ import { uspacySdk } from '../index';
 	try {
 		await uspacySdk.authService.login({ email: process.env.LOGIN_EMAIL, password: process.env.LOGIN_PASSWORD, remember: true });
 		await uspacySdk.tasksService.getFiltersPresets();
-		const a = await uspacySdk.tasksService.getTasksSettings();
-		// eslint-disable-next-line no-console
-		console.log(a.data.table_settings.tasks.column_ordering);
+		await uspacySdk.tasksService.getTasksSettings();
 		// uspacySdk.tasksService.getFiltersPreset(d.data.docs[0]._id);
 		// uspacySdk.tasksService.updateFilterPreset(d.data.docs[0]._id, d.data.docs[0]._rev, { aa: 'bb111' });
 		// uspacySdk.tasksService.createFilterPreset({ aa: 'bb' });
