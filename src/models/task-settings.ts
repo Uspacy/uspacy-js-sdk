32d71ff8444683da49920d88725bf3ea04238944
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2024-07-17 14:15:08 +0300

    Added ITaskSettings model

---
+++ src/models/task-settings.ts
@@ -0,0 +1,24 @@
+export interface IColumnVisibility {
+	[key: string]: boolean;
+}
+
+export interface IColumnSizes {
+	[key: string]: number;
+}
+
+export interface ITaskColumnSettings {
+	column_visibility: IColumnVisibility;
+	column_sizes: IColumnSizes;
+	column_ordering: string[];
+}
+
+export interface ITableSettings {
+	table_settings: {
+		[key: string]: ITaskColumnSettings;
+	};
+}
+
+export interface ITaskSettings extends ITableSettings {
+	_id: string;
+	_rev: string;
+}
