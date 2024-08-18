676456b3e9e99e4d524d5c254ab0d2f1e02b3583
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2024-08-18 18:21:25 +0300

    Added google calendars methods and models types

---
+++ src/models/calendars.ts
@@ -0,0 +1,48 @@
+export interface IGoogleCalendarsAccount {
+	id: number;
+	email: string;
+	provider: string;
+	title: string;
+	timezone: string;
+	sync_types: string;
+	activity_type: string;
+	last_sync_at: string;
+	direction: string;
+	selected: number;
+	description: string;
+	private_event: number;
+	remote_calendar_id: string;
+	picture: string;
+}
+
+export interface IGoogleCalendar {
+	title: string;
+	description: string;
+	remote_calendar_id: string;
+	role: string;
+	timeZone: string;
+}
+
+export interface IAfterGoodleOauth2Response {
+	domain: string;
+	is_open_auth_calendar_drawer: boolean;
+}
+
+export interface ICalendarsSuccessResponse {
+	status: boolean;
+	message: string;
+}
+
+export enum ECalendarDirectionType {
+	DOUBLE_SIDED = 'double_sided',
+	PROVIDER_ACTIVITY = 'provider_activity',
+	ACTIVITY_PROVIDER = 'activity_provider',
+}
+
+export enum EEventsValues {
+	ACTIVITY = 'activity',
+	CALL = 'call',
+	MEETING = 'meeting',
+	CHAT = 'chat',
+	MAIL = 'mail',
+}
