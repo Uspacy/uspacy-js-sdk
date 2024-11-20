4bdef6bbf4efb30246785de531671e07395dee56
MysikM <mishamysik@gmail.com>
2024-11-20 10:36:08 +0200

    change route

--- src/services/AnalyticsService/index.ts
+++ src/services/AnalyticsService/index.ts
@@ -18,7 +18,7 @@ export class AnalyticsService {
 	 * @returns Array analytics report list
 	 */
 	getAnalyticsReportList(params: string, signal: AbortSignal) {
-		return this.httpClient.client.get<IAnalyticReportList>(`${this.namespace}/reports?${params}`, {
+		return this.httpClient.client.get<IAnalyticReportList>(`${this.namespace}/reports?page=1&list=10`, {
 			signal: signal,
 		});
 	}
