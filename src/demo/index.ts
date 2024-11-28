c3dcfb284fdcd9b8b45700a60b25b075c8475090
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2024-11-28 17:41:00 +0200

    Updated unicode symbol

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -3,8 +3,8 @@ import { uspacySdk } from '../index';
 (async () => {
 	try {
 		await uspacySdk.authService.login({ email: process.env.LOGIN_EMAIL, password: process.env.LOGIN_PASSWORD, remember: true });
-		// await uspacySdk.tasksService.getFiltersPresets('tasks');
-		// await uspacySdk.tasksService.getTasksSettings();
+		await uspacySdk.tasksService.getFiltersPresets('tasks');
+		// await uspacySdk.tasksService.getTasksSettings('task');
 		// uspacySdk.tasksService.getFiltersPreset(d.data.docs[0]._id);
 		// uspacySdk.tasksService.updateFilterPreset(d.data.docs[0]._id, d.data.docs[0]._rev, { aa: 'bb111' });
 		// uspacySdk.tasksService.createFilterPreset({ aa: 'bb' });
