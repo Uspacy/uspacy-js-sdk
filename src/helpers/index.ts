f4d0dfeaf84a1351f9aa3b2e40c79a6bf58dca8a
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2025-03-21 12:45:34 +0200

    domain fix

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,6 @@
 import { ResourceType } from '../models/resources';
 
 export const getResourcesDomain = (type: ResourceType) =>
-	window.location.origin.includes('stage') ? `https://${type}s.staging.uspacy.tech` : `https://${type}s.uspa.cy`;
+	window.location.origin.includes('stage') || window.location.origin.includes('localhost')
+		? `https://${type}s.staging.uspacy.tech`
+		: `https://${type}s.uspa.cy`;
