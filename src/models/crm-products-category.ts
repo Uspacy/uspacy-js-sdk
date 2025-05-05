4f5a4693ba933a68c560b7c61728d474f77e3b03
MysikM <mishamysik@gmail.com>
2025-05-05 18:48:30 +0300

    update categories interface

--- src/models/crm-products-category.ts
+++ src/models/crm-products-category.ts
@@ -3,16 +3,10 @@ export interface IProductCategory {
 	parent_id: number;
 	name: string;
 	is_active?: number;
+	collapsed?: number;
 	child_categories?: IProductCategory[];
 }
 
 export interface IProductCategories {
-	data: {
-		id: number;
-		parent_id: number;
-		name: string;
-		is_active?: number;
-
-		child_categories?: IProductCategory[];
-	}[];
+	data: IProductCategory[];
 }
