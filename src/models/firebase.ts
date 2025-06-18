9d9d5fd950832adaa20e75cb26d4f2a2129478a1
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2025-06-18 10:12:04 +0300

    Added firebase types

---
+++ src/models/firebase.ts
@@ -0,0 +1,20 @@
+export enum EFirebaseDbId {
+	SETTINGS_STAGE = 'settings-stage',
+	SETTINGS = 'settings',
+}
+
+export enum EFirabaseService {
+	CRM = 'crm',
+	TASKS = 'tasks',
+	USERS = 'users',
+}
+
+export enum EFirabaseDbName {
+	CRM_SETTINGS = 'crm-settings',
+	CRM_PRESETS = 'crm-presets',
+	CRM_KANBAN_COLUMN_VISIBILITIES = 'crm-kanban-column-visibilities',
+	CRM_ENTITY_CARD_SETTINGS = 'crm-entity-card-settings',
+	TASKS_SETTINGS = 'tasks-settings',
+	TASKS_PRESETS = 'tasks-presets',
+	TASKS_KANBAN_COLUMN_VISIBILITIES = 'tasks-kanban-column-visibilities',
+}
