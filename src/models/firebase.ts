7adfb888dd50e3f808881e2e379820028dec35e8
AndreyMalyshev147 <andrey.malyshev.97@gmail.com>
2025-06-26 17:39:50 +0300

    Updated firebase types

--- src/models/firebase.ts
+++ src/models/firebase.ts
@@ -1,6 +1,7 @@
 export enum EFirebaseDbId {
 	SETTINGS_STAGE = 'settings-stage',
 	SETTINGS = 'settings',
+	DEFAULT = 'default',
 }
 
 export enum EFirabaseService {
@@ -14,6 +15,7 @@ export enum EFirabaseDbName {
 	CRM_PRESETS = 'crm-presets',
 	CRM_KANBAN_COLUMN_VISIBILITIES = 'crm-kanban-column-visibilities',
 	CRM_ENTITY_CARD_SETTINGS = 'crm-entity-card-settings',
+	TASKS_ENTITY_CARD_SETTINGS = 'tasks-entity-card-settings',
 	TASKS_SETTINGS = 'tasks-settings',
 	TASKS_PRESETS = 'tasks-presets',
 	TASKS_KANBAN_COLUMN_VISIBILITIES = 'tasks-kanban-column-visibilities',
