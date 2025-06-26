b4a6dbe12c656dd9409af5bb56bf8a544a638b05
MysikM <mishamysik@gmail.com>
2025-06-26 10:42:09 +0300

    resolve conflict

--- src/services/TasksService/index.ts
+++ src/services/TasksService/index.ts
@@ -556,4 +556,47 @@ export class TasksService {
 			urlParams: { fieldCode },
 		});
 	}
+
+	/**
+	 * Get deleted(trash) task
+	 * @param id item id
+	 * @returns activity item
+	 */
+	getTrashTask(id: number) {
+		return this.httpClient.client.get<ITask>(`${this.namespaceTrashTasks}`, {
+			params: {
+				id,
+			},
+		});
+	}
+
+	/**
+	 * Restore tasks
+	 * @param itemIds restore items by ids
+	 * @param all all items restore
+	 * @param exceptIds items that don't need to be restored
+	 */
+	restoreTrashTasks({ itemIds, all, exceptIds }: { itemIds: number[]; all: boolean; exceptIds: number[] }) {
+		return this.httpClient.client.patch(`${this.namespaceTrashTasks}/restore`, {
+			id: itemIds,
+			all,
+			except_ids: exceptIds,
+		});
+	}
+
+	/**
+	 * Remove from basket tasks
+	 * @param itemIds delete items by ids
+	 * @param all all items delete
+	 * @param exceptIds items that don't need to be delete
+	 */
+	deleteTrashTasks({ itemIds, all, exceptIds }: { itemIds: number[]; all: boolean; exceptIds: number[] }) {
+		return this.httpClient.client.delete(`${this.namespaceTrashTasks}`, {
+			data: {
+				entity_id: itemIds,
+				all,
+				except_ids: exceptIds,
+			},
+		});
+	}
 }
