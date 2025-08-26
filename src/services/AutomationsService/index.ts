0fa1ce8c68a94e3b592e07e89773caf9ec7926db
MysikM <mishamysik@gmail.com>
2025-08-26 13:14:53 +0300

    add workflows

--- src/services/AutomationsService/index.ts
+++ src/services/AutomationsService/index.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '../../core/HttpClient';
 import { IApp } from '../../models/app';
 import { IAutomation } from '../../models/automations';
 import { IResponseWithMeta } from '../../models/response';
+import { IWorkflowsResponse } from '../../models/workflows';
 
 /**
  * Automations service
@@ -11,6 +12,8 @@ import { IResponseWithMeta } from '../../models/response';
 @injectable()
 export class AutomationsService {
 	private namespace = '/automations-backend/v1/';
+	private namespace_workers = '/automations-backend/v1/workers';
+	private namespace_workflows = '/automations-backend/v1/processes';
 	constructor(private readonly httpClient: HttpClient) {}
 
 	/**
@@ -20,7 +23,7 @@ export class AutomationsService {
 	 * @param search search query
 	 */
 	async getAutomations(page?: number, list?: number, search?: string) {
-		return this.httpClient.client.get<IResponseWithMeta<IApp[]>>(this.namespace, {
+		return this.httpClient.client.get<IResponseWithMeta<IApp[]>>(this.namespace_workers, {
 			params: {
 				page,
 				list,
@@ -34,7 +37,7 @@ export class AutomationsService {
 	 * @param id automation id
 	 */
 	deleteAutomation(id: number) {
-		return this.httpClient.client.delete<number>(`${this.namespace}:id`, { urlParams: { id } });
+		return this.httpClient.client.delete<number>(`${this.namespace_workers}:id`, { urlParams: { id } });
 	}
 
 	/**
@@ -43,6 +46,39 @@ export class AutomationsService {
 	 * @param body request body
 	 */
 	toggleAutomation(id: number, body: IAutomation) {
-		return this.httpClient.client.patch(`${this.namespace}:id`, body, { urlParams: { id } });
+		return this.httpClient.client.patch(`${this.namespace_workers}:id`, body, { urlParams: { id } });
+	}
+
+	/**
+	 * Get processes list
+	 * @param page page number
+	 * @param list page count
+	 * @param search search query
+	 */
+	async getWorkflows(page?: number, list?: number, search?: string) {
+		return this.httpClient.client.get<IWorkflowsResponse>(this.namespace_workflows, {
+			params: {
+				page,
+				list,
+				search,
+			},
+		});
+	}
+
+	/**
+	 * Delete workflow
+	 * @param id workflow id
+	 */
+	deleteWorkflow(id: number) {
+		return this.httpClient.client.delete<number>(`${this.namespace_workflows}:id`, { urlParams: { id } });
+	}
+
+	/**
+	 * Toggle workflow
+	 * @param id workflow id
+	 * @param body request body
+	 */
+	toggleWorkflow(id: number, body: IAutomation) {
+		return this.httpClient.client.patch(`${this.namespace_workflows}:id`, body, { urlParams: { id } });
 	}
 }
