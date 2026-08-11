ded7c4576ea60f796f2ae0c19d59dd63f22b3c41
MysikM <mishamysik@gmail.com>
2026-08-11 09:21:17 +0300

    update interface

--- src/models/contributors.ts
+++ src/models/contributors.ts
@@ -1,6 +1,13 @@
+export type ContributorStatus = 'ok' | 'not_found' | 'forbidden';
+
 export interface IContributor {
+	status: ContributorStatus;
 	portal_id: number;
 	title: string;
+	deleted_at: string | null;
+	created_by: number;
+	updated_by: number;
+	active: boolean;
 }
 
 export type IContributorsLookupRequest = Record<string, number[]>;
