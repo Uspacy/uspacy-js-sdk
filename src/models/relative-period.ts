0a2765c69c39d606f7c7f2567083d80b337df640
MysikM <mishamysik@gmail.com>
2026-08-21 12:25:55 +0300

    update periods

--- src/models/relative-period.ts
+++ src/models/relative-period.ts
@@ -1,6 +1,6 @@
 export const RELATIVE_PERIOD_LABEL = 'customPeriod';
 
-export type RelativePeriodDirection = 'past' | 'current' | 'future';
+export type RelativePeriodDirection = 'past' | 'current' | 'future' | 'last' | 'next';
 export type RelativePeriodUnit = 'day' | 'week' | 'month' | 'year';
 
 export interface IRelativePeriod {
